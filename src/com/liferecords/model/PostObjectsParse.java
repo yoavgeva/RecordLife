bc1647a79ea482c6a669955d78fe0623f11b33e1
yoavgeva <dryoavgeva@gmail.com>
2014-09-01 22:35:03 +0300

    app: arrange postObjectsParse class

--- src/com/liferecords/model/PostObjectsParse.java
+++ src/com/liferecords/model/PostObjectsParse.java
@@ -1,47 +1,50 @@
 package com.liferecords.model;
 
-import java.io.ObjectInputStream.GetField;
 import java.util.Date;
 
 import com.parse.ParseObject;
 import com.parse.ParseUser;
 
 public class PostObjectsParse extends ParseObject {
-	double latitude;
-	double longitude;
-	double accuracy;
-	
-	public ParseUser getUser(){
-		return getParseUser("user");		
+
+	public ParseUser getUser() {
+		return getParseUser("user");
 	}
-	public void setUser(){
-		put("user",ParseUser.getCurrentUser());
+
+	public void setUser() {
+		put("user", ParseUser.getCurrentUser());
 	}
+
 	public double getLatitude() {
 		return getDouble("latitude");
 	}
+
 	public void setLatitude(double latitude) {
-		put("latitude",latitude);
+		put("latitude", latitude);
 	}
+
 	public double getLongitude() {
 		return getDouble("longitude");
 	}
+
 	public void setLongitude(double longitude) {
-		put("longitude",longitude);
+		put("longitude", longitude);
 	}
+
 	public double getAccuracy() {
 		return getDouble("accuracy");
 	}
+
 	public void setAccuracy(float accuracy) {
-		put("accuracy",accuracy);
+		put("accuracy", accuracy);
 	}
-	
-	public Date getDateInstance(){
+
+	public Date getDateInstance() {
 		return getDate("date");
 	}
-	public void setDate(Date dateInstance){
-		put("date",dateInstance);
+
+	public void setDate(Date dateInstance) {
+		put("date", dateInstance);
 	}
-	
 
 }
