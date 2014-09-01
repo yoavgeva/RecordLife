9aa0a69c6d9ef107060f07227eea33d493237a77
yoavgeva <dryoavgeva@gmail.com>
2014-09-01 20:19:50 +0300

    app: fixed location bug more

--- src/service/LocationServ.java
+++ src/service/LocationServ.java
@@ -1,20 +1,20 @@
 package service;
 
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient;
 import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.liferecords.application.HistoryData;
-
-import android.app.Service;
-import android.content.Intent;
-import android.location.Location;
-import android.location.LocationManager;
-import android.os.Bundle;
-import android.os.IBinder;
-import android.support.v4.content.LocalBroadcastManager;
-import android.util.Log;
 
 public class LocationServ extends Service implements LocationListener,
 		GooglePlayServicesClient.ConnectionCallbacks,
@@ -23,8 +23,6 @@ public class LocationServ extends Service implements LocationListener,
 	static final String TAG = LocationServ.class.getSimpleName();
 	public static final String BROADCASTACTION = "Location";
 	private static final int TIME = 1000 * 60 / 6; // type the minutes last
-	public LocationManager locationManager;
-	public Location previousBestLocation = null;
 	LocationRequest locationRequest;
 	LocationClient locationClient;
 	Intent intent;
@@ -34,6 +32,8 @@ public class LocationServ extends Service implements LocationListener,
 	@Override
 	public void onCreate() {
 
+		Context content = this;
+		account = new HistoryData(content);
 		locationRequest = LocationRequest.create();
 		locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 		locationRequest.setInterval(TIME);
@@ -80,18 +80,16 @@ public class LocationServ extends Service implements LocationListener,
 
 	private void updateLocation(Location location){
 		Log.d(TAG, "Location: " + location);
-		if(location == null){
-			return;
+		if(location != null){
+			double latitude = location.getLatitude();
+			double longitude = location.getLongitude();
+			float accuracy = location.getAccuracy();
+			account.updateGeo(latitude, longitude, accuracy);
+			intent = new Intent(BROADCASTACTION);
+			LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
 		}
-		double latitude = location.getLatitude();
-		double longitude = location.getLongitude();
-		double accuracy = location.getAccuracy();
-		account.setLatitude(latitude);
-		account.setLongitude(longitude);
-		account.setAccuracy(accuracy);
-		intent = new Intent(BROADCASTACTION);
-		LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
-		
+				
 		
 	}
+	
 }
