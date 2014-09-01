7dd280a69a5542e4ea0d1289f44fa3879276d9b9
yoavgeva <dryoavgeva@gmail.com>
2014-09-01 15:49:51 +0300

    app: location service ready to check

---
+++ src/com/liferecords/application/HistoryData.java
@@ -0,0 +1,37 @@
+package com.liferecords.application;
+
+public class HistoryData {
+	private Double latitude;
+	private Double longitude;
+	private Double accuracy;
+	private String address;
+	
+	
+	
+	
+	public Double getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
+	}
+	public Double getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(Double longitude) {
+		this.longitude = longitude;
+	}
+	public Double getAccuracy() {
+		return accuracy;
+	}
+	public void setAccuracy(Double accuracy) {
+		this.accuracy = accuracy;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
--- src/service/LocationServ.java
+++ src/service/LocationServ.java
@@ -5,6 +5,7 @@ import com.google.android.gms.common.GooglePlayServicesClient;
 import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
+import com.liferecords.application.HistoryData;
 
 import android.app.Service;
 import android.content.Intent;
@@ -12,9 +13,14 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 
-public class LocationServ extends Service implements LocationListener,GooglePlayServicesClient.ConnectionCallbacks,GooglePlayServicesClient.OnConnectionFailedListener{
+public class LocationServ extends Service implements LocationListener,
+		GooglePlayServicesClient.ConnectionCallbacks,
+		GooglePlayServicesClient.OnConnectionFailedListener {
 
+	static final String TAG = LocationServ.class.getSimpleName();
 	public static final String BROADCASTACTION = "Location";
 	private static final int TIME = 1000 * 60 / 6; // type the minutes last
 	public LocationManager locationManager;
@@ -22,58 +28,70 @@ public class LocationServ extends Service implements LocationListener,GooglePlay
 	LocationRequest locationRequest;
 	LocationClient locationClient;
 	Intent intent;
+	HistoryData account;
 	int counter = 0;
 
 	@Override
 	public void onCreate() {
-		
+
 		locationRequest = LocationRequest.create();
 		locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 		locationRequest.setInterval(TIME);
 		locationRequest.setFastestInterval(TIME);
 		locationClient = new LocationClient(this, this, this);
 		locationClient.connect();
-		
+
 	}
 
-	
+	@Override
+	public void onDestroy() {
+		if (locationClient.isConnected()) {
+			locationClient.removeLocationUpdates(this);
+			locationClient.disconnect();
+		}
+
+	}
 
 	@Override
 	public IBinder onBind(Intent intent) {
-
 		return null;
 	}
 
-
-
 	@Override
 	public void onLocationChanged(Location arg0) {
-		// TODO Auto-generated method stub
-		
+		updateLocation(arg0);
 	}
-
-
 
 	@Override
 	public void onConnectionFailed(ConnectionResult arg0) {
-		// TODO Auto-generated method stub
-		
+		stopSelf();
 	}
-
-
 
 	@Override
 	public void onConnected(Bundle arg0) {
-		// TODO Auto-generated method stub
-		
+		locationClient.requestLocationUpdates(locationRequest, this);
 	}
-
-
 
 	@Override
 	public void onDisconnected() {
-		// TODO Auto-generated method stub
-		
+		locationClient.removeLocationUpdates(this);
+		stopSelf();
 	}
 
+	private void updateLocation(Location location){
+		Log.d(TAG, "Location: " + location);
+		if(location == null){
+			return;
+		}
+		double latitude = location.getLatitude();
+		double longitude = location.getLongitude();
+		double accuracy = location.getAccuracy();
+		account.setLatitude(latitude);
+		account.setLongitude(longitude);
+		account.setAccuracy(accuracy);
+		intent = new Intent(BROADCASTACTION);
+		LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+		
+		
+	}
 }
