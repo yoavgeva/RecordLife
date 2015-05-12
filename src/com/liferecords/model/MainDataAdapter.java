43a8bf3fcd58dee046b0fe7998c96529107a8530
yoavgeva <dryoavgeva@gmail.com>
2015-05-12 19:59:33 +0300

    app: finished first draft of adapter

--- src/com/liferecords/model/MainDataAdapter.java
+++ src/com/liferecords/model/MainDataAdapter.java
@@ -4,60 +4,66 @@ package com.liferecords.model;
 import java.util.List;
 import java.util.TreeMap;
 
+import com.liferecords.application.R;
+
+
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
+import android.widget.CheckedTextView;
+import android.widget.TextView;
 
 public class MainDataAdapter extends BaseExpandableListAdapter {
-	
-	
+
+
 	private final Context context;
 	private final Model model;	
 	private List<DateAdapterItem> itemsGroup;
 	private TreeMap<DateAdapterItem,List<ModelAdapterItem>> itemsChildren;
-	
-	
+
+
 	public MainDataAdapter(Context context){
 		this.context = context;
 		this.model = new Model(this.context);
 		populate();
 	}
-	
+
 	@Override
 	public int getGroupCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		return this.itemsGroup.size();
 	}
 
 	@Override
 	public int getChildrenCount(int groupPosition) {
-		// TODO Auto-generated method stub
-		return 0;
+
+		return this.itemsChildren.get(this.itemsGroup.get(groupPosition)).size();
 	}
 
 	@Override
 	public Object getGroup(int groupPosition) {
-		// TODO Auto-generated method stub
-		return null;
+
+		return this.itemsGroup.get(groupPosition);
 	}
 
 	@Override
 	public Object getChild(int groupPosition, int childPosition) {
-		// TODO Auto-generated method stub
-		return null;
+
+		return this.itemsChildren.get(this.itemsGroup.get(groupPosition)).get(childPosition);
 	}
 
 	@Override
 	public long getGroupId(int groupPosition) {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		return groupPosition;
 	}
 
 	@Override
 	public long getChildId(int groupPosition, int childPosition) {
-		// TODO Auto-generated method stub
-		return 0;
+
+		return childPosition;
 	}
 
 	@Override
@@ -66,35 +72,53 @@ public class MainDataAdapter extends BaseExpandableListAdapter {
 		return false;
 	}
 
+	public DateAdapterItem getGroupItems(int position){
+		return itemsGroup.get(position);
+	}
 	@Override
 	public View getGroupView(int groupPosition, boolean isExpanded,
 			View convertView, ViewGroup parent) {
-		// TODO Auto-generated method stub
-		return null;
+		if(convertView == null){
+			LayoutInflater inflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			convertView = inflater.inflate(R.layout.listrow_group, parent,false);
+		}
+		DateAdapterItem item = getGroupItems(groupPosition);
+		CheckedTextView txtGroup = (CheckedTextView) convertView.findViewById(R.id.checked_textview_group);
+		return convertView;
 	}
 
+	public ModelAdapterItem getChildItems(int position){
+		return (ModelAdapterItem) itemsChildren.get(position);
+		
+	}
 	@Override
 	public View getChildView(int groupPosition, int childPosition,
 			boolean isLastChild, View convertView, ViewGroup parent) {
-		// TODO Auto-generated method stub
-		return null;
+		if(convertView == null){
+			LayoutInflater inflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			convertView = inflater.inflate(R.layout.listrow_details, parent,false);
+		}
+		ModelAdapterItem item = getChildItems(childPosition);
+		TextView  txtChild = (TextView) convertView.findViewById(R.id.textViewDetails);
+		txtChild.setText(item.address);
+		return convertView;
 	}
 
 	@Override
 	public boolean isChildSelectable(int groupPosition, int childPosition) {
 		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
-	
+
 	private void populate(){
 		this.itemsGroup = this.model.getDateAdapterItems();
-		
+
 		for (int i = 0; i <= this.itemsGroup.size(); i++) {
 			DateAdapterItem groupObject = (DateAdapterItem)this.itemsGroup.get(i);
 			this.itemsChildren.put(this.itemsGroup.get(i), this.model.getDataDateAdapterItems(groupObject.dateWithoutTime));
 		}
-		
-		
+
+
 	}
 
 }
